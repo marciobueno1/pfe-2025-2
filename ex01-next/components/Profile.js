c7c2e25729edcf304381651c0b3c9e063e97e7d5
Marcio Bueno <marciobueno@gmail.com>
2025-08-15 00:35:05 +0000

    Primeiro componente React

---
+++ ex01-next/components/Profile.js
@@ -0,0 +1,10 @@
+export function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      height={200}
+      width={200}
+      alt="Katherine Johnson"
+    />
+  );
+}
