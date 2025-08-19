c81a3ad945c406f026e60eb2d162a5ebc8d5f906
Marcio Bueno <marciobueno@gmail.com>
2025-08-19 00:23:11 +0000

    Conversão HTML para JSX + utilização de chaves em JSX

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,15 +1,21 @@
 //import Image from "next/image";
 //import styles from "./page.module.css";
 
+import { Avatar } from "@/components/Avatar";
 import { Profile } from "@/components/Profile";
+import { TodoList } from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div>
+    <section>
       <h1>Amazing scientists</h1>
       <Profile />
       <Profile />
       <Profile />
-    </div>
+      <hr />
+      <TodoList />
+      <hr />
+      <Avatar />
+    </section>
   );
 }
---
+++ ex01-next/components/TodoList.js
@@ -0,0 +1,23 @@
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
+
+export function TodoList() {
+  const name = "Hedy Lamarr";
+  const photo = "https://i.imgur.com/yXOvdOSs.jpg";
+  return (
+    <>
+      <h1>
+        {name}'s Todos for {formatDate(today)}
+      </h1>
+      <img src={photo} alt={name} className="photo" />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
