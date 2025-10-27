a27e91fc5cb74e085de0d970e2ba2838e4130430
Marcio Bueno <marciobueno@gmail.com>
2025-10-27 23:56:20 +0000

    Adicionando o zustand persist

--- ex01-next/zustand/index.js
+++ ex01-next/zustand/index.js
@@ -1,10 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useStore = create((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-  clear: () => set({ count: 0 }),
-  add: (n) => set((state) => ({ count: state.count + n })),
-  darkTheme: false,
-  toggleTheme: () => set((state) => ({ darkTheme: !state.darkTheme })),
-}));
+export const useStore = create(
+  persist(
+    (set) => ({
+      count: 1,
+      inc: () => set((state) => ({ count: state.count + 1 })),
+      clear: () => set({ count: 0 }),
+      add: (n) => set((state) => ({ count: state.count + n })),
+      darkTheme: false,
+      toggleTheme: () => set((state) => ({ darkTheme: !state.darkTheme })),
+    }),
+    {
+      name: "pfe20252-counter-storage", // unique name
+    }
+  )
+);
